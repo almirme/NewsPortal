38ede6aa5220c157f2038dcb065bb68fda5422a7
Almir Mehanović <68317839+almirme@users.noreply.github.com>
2020-08-12 06:32:45 +0200

    Clean up project

--- NewsPortal.Tests/Controllers/HomeControllerTest.cs
+++ NewsPortal.Tests/Controllers/HomeControllerTest.cs
@@ -13,41 +13,12 @@ namespace NewsPortal.Tests.Controllers
     public class HomeControllerTest
     {
         [TestMethod]
-        public void Index()
+        public void Index_OnRequest_ReturnsView()
         {
-            // Arrange
             HomeController controller = new HomeController();
 
-            // Act
             ViewResult result = controller.Index() as ViewResult;
 
-            // Assert
-            Assert.IsNotNull(result);
-        }
-
-        [TestMethod]
-        public void About()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.About() as ViewResult;
-
-            // Assert
-            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
-        }
-
-        [TestMethod]
-        public void Contact()
-        {
-            // Arrange
-            HomeController controller = new HomeController();
-
-            // Act
-            ViewResult result = controller.Contact() as ViewResult;
-
-            // Assert
             Assert.IsNotNull(result);
         }
     }
