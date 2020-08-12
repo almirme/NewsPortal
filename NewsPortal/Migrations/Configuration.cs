f77036fee67b6615adc796173c83ad7565926314
Almir Mehanović <68317839+almirme@users.noreply.github.com>
2020-08-12 08:32:51 +0200

    Enable migrations

---
+++ NewsPortal/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+namespace NewsPortal.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<NewsPortal.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "NewsPortal.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(NewsPortal.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
