990f9df80c38ee524c0fe4afea323fab9d24e487
Almir Mehanović <68317839+almirme@users.noreply.github.com>
2020-08-12 08:29:58 +0200

    Add interface for repository of news articles

---
+++ NewsPortal/Models/INewsRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewsPortal.Models
+{
+    interface INewsRepository
+    {
+        IEnumerable<NewsArticle> GetLatest(int numberOfLatestNews);
+    }
+}
