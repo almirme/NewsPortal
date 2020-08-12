a18ac04a65c0950ebaaf239df38e6a59a779edc2
Almir Mehanović <68317839+almirme@users.noreply.github.com>
2020-08-12 06:50:14 +0200

    Add basic model for new article

---
+++ NewsPortal/Models/NewsArticle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NewsPortal.Models
+{
+    public class NewsArticle
+    {
+        public long Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public DateTime PublishDate { get; set; }
+
+        public string Content { get; set; }
+
+        public string PictureUrl { get; set; }
+
+        public string Category { get; set; }
+    }
+}
