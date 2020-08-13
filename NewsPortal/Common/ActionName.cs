8b8b4f96009ced16a53a923ef21e5b0fc4b3fb19
Almir Mehanović <68317839+almirme@users.noreply.github.com>
2020-08-13 03:56:52 +0200

    Refactor and format code

--- NewsPortal/Common/ActionName.cs
+++ NewsPortal/Common/ActionName.cs
@@ -9,6 +9,7 @@ namespace NewsPortal.Common
     {
         public static string Home_Index => "Index";
         public static string Home_NewsList => "NewsList";
+        public static string Home_SingleNews => "SingleNews";
         public static string Home_NewsInCategory => "NewsInCategory";
         public static string NewsAdmin_NewNews => "NewNews";
         public static string NewsAdmin_EditNews => "EditNews";
